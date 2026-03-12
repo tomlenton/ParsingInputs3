a3262052ef2c0ae511695d4f0f49c3d7c9337fb8
tomlenton <twlenton@gmail.com>
2026-03-12 16:39:39 +0000

    Add .gitignore

---
+++ NorthcodersStudents/NorthcodersStudent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthcodersStudents
+{
+    class NorthcodersStudent
+    {
+        public string Name { get; set; }
+        public int YearsCoding { get; set; }
+        public bool IsDebugging { get; set; }
+        public string CodingAffiliation { get; set; } = "Northcoders";
+        public void CheckIfDebugging()
+        {
+            if (IsDebugging)
+            {
+                Console.WriteLine("Defeating bugs in code...");
+            }
+            else
+            {
+                Console.WriteLine("Bugs defeated, happy coding!");
+            }
+        }
+        public NorthcodersStudent( string name, int yearsCoding, bool isDebugging, string codingAffiliation)
+        {
+            Name = name;
+            YearsCoding = yearsCoding;
+            IsDebugging = isDebugging;
+            CodingAffiliation = codingAffiliation;
+
+        }
+    }
+}
+
---
+++ NorthcodersStudents/NorthcodersSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace NorthcodersStudents
+{
+    internal class NorthcodersSystem
+    {
+
+        static List<NorthcodersStudent> student = new List<NorthcodersStudent>();
+        static void Main(string[] args)
+        {
+
+            string json = "{ \"Name\": \"Evie Pom\", \"YearsCoding\": 6, \"IsDebugging\": false, \"CodingAffiliation\" : \"Poms Who Code\"}";
+
+            NorthcodersStudent? jason = JsonSerializer.Deserialize<NorthcodersStudent>(json);
+
+            Console.WriteLine(jason.Name);
+
+
+        }
+    }
+}
